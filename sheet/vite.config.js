819904397b36e7d93ff3c722f62f4818db95f808
AndreaPetracca <andrea.petracca@vidyasoft.it>
2026-03-17 13:50:51 +0100

    update release v1.0.0

--- sheet/vite.config.js
+++ sheet/vite.config.js
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ mode }) => {
+  // Questa riga controlla se stiamo compilando su GitHub
   const isGithub = process.env.GITHUB_ACTIONS === 'true';
 
   return {
+    // SE siamo su GitHub usa il percorso della sottocartella
+    // ALTRIMENTI usa il percorso relativo './' (che serve a Electron per non essere bianco)
     base: isGithub ? '/asgaroth_dnd_sheet_apk/' : './',
+    
     plugins: [
       react(),
       VitePWA({
@@ -15,15 +19,22 @@ export default defineConfig(({ mode }) => {
           name: 'Asgaroth D&D Sheet',
           short_name: 'Scheda D&D',
           display: 'standalone',
-          start_url: '.', 
-          icons: [{ src: 'icon.svg', sizes: 'any', type: 'image/svg+xml' }]
+          start_url: './index.html',
+          icons: [
+            {
+              src: 'icon.svg',
+              sizes: 'any',
+              type: 'image/svg+xml',
+              purpose: 'any maskable'
+            }
+          ]
         }
       })
     ],
     build: {
       outDir: 'dist',
       assetsDir: 'assets',
-      emptyOutDir: true 
+      emptyOutDir: true
     }
   }
 })
