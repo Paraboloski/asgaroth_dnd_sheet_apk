dae9caae3110d8e6ab62603662c3115fbfb0bef8
AndreaPetracca <andrea.petracca@vidyasoft.it>
2026-03-17 12:19:03 +0100

    update release v1.0.0

--- sheet/vite.config.js
+++ sheet/vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', 
+      manifest: {
+        name: 'Asgaroth D&D Sheet',
+        short_name: 'Scheda D&D',
+        description: '',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone', 
+        icons: [
+          {
+            src: 'icon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   base: './', 
 })
